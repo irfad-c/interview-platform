3c12009636d193418aa65b39273c3cdc6d79e2b5
Irfad C <irfadc500@gmail.com>
2025-12-08 14:49:17 +0530

    Add package.json file at the root folder with build scripts

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,4 +7,4 @@ app.get("/", (req, res) => {
   res.status(200).json({ message: "success from backend" });
 });
 
-app.listen(ENV.PORT, () => console.log("Server running on port:",ENV.PORT));
+app.listen(ENV.PORT, () => console.log("Server running on port:", ENV.PORT));
