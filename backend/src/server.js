f8c586a38b93c31b69c1097a6e936ed793d3af09
Irfad C <irfadc500@gmail.com>
2025-12-09 18:23:37 +0530

    Update configuration for successful backend deployment on Render

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,19 +8,21 @@ app.get("/", (req, res) => {
   res.status(200).json({ message: "success from backend" });
 });
 
-app.get("/something",(req,res)=>{
-  res.status(200).json({message:"This is something"})
-})
+app.get("/something", (req, res) => {
+  res.status(200).json({ message: "This is something" });
+});
 
-const __dirname=path.resolve();
+const __dirname = path.resolve();
 
-if(ENV.NODE_ENV==="production"){
+if (ENV.NODE_ENV === "production") {
   //Serve the built frontend files (React/Vite) to the user
-app.use(express.static(path.join(__dirname,"../frontend/dist")))
-//This is a catch-all route
-app.get("/{*any}",(req,res)=>{
-    res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
-  })
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  //This is a catch-all route
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
 
-app.listen(ENV.PORT, () => console.log("Server running on port:", ENV.PORT));
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => console.log("Server running on port:", PORT));
