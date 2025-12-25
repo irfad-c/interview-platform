fa7efb0651fce145218cd571e3899e85612634c4
Irfad C <irfadc500@gmail.com>
2025-12-25 15:26:13 +0530

    Fix route parameters by replacing :/id with /:id

--- backend/src/routes/sessionRoutes.js
+++ backend/src/routes/sessionRoutes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {
   createSession,
+  endSession,
   getActiveSession,
   getMyRecentSession,
   getSessionById,
@@ -12,7 +13,7 @@ const router = express.Router();
 router.post("/", protectRoute, createSession);
 router.get("/active", protectRoute, getActiveSession);
 router.get("/my-recent", protectRoute, getMyRecentSession);
-router.get(":/id", protectRoute, getSessionById);
-router.post(":/id/join", protectRoute, joinSession);
-router.post(":/id/end", protectRoute, endSession);
+router.get("/:id", protectRoute, getSessionById);
+router.post("/:id/join", protectRoute, joinSession);
+router.post("/:id/end", protectRoute, endSession);
 export default router;
