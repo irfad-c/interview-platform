9fbdf6535a2487ae731e58ef58602203abfeff85
Irfad C <irfadc500@gmail.com>
2026-01-15 16:55:45 +0530

    Integrate ProblemDescription in to the ProblemPage

--- frontend/src/pages/ProblemPage.jsx
+++ frontend/src/pages/ProblemPage.jsx
@@ -1,10 +1,34 @@
+import { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router";
+import { PROBLEMS } from "../data/problems";
+import Navbar from "../components/Navbar";
 import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
 import ProblemDescription from "../components/ProblemDescription";
 import OutputPanel from "../components/OutputPanel";
 import CodeEditorPanel from "../components/CodeEditorPanel";
-import Navbar from "../components/Navbar";
 
 function ProblemPage() {
+  const navigate = useNavigate();
+  const [currentProblemId, setCurrentProblemId] = useState("two-sum");
+  const { id } = useParams();
+  const [output, setOutput] = useState(null);
+
+  const currentProblem = PROBLEMS[currentProblemId];
+  if (!currentProblem) {
+    return <div>Problem not found</div>;
+  }
+
+  const handleProblemChange = (newProblemId) =>
+    navigate(`/problem/${newProblemId}`);
+
+  // update problem when URL param changes
+  useEffect(() => {
+    if (id && PROBLEMS[id]) {
+      setCurrentProblemId(id);
+      setOutput(null);
+    }
+  }, [id]);
+
   return (
     <div className="h-screen bg-base-100 flex flex-col">
       <Navbar />
@@ -12,7 +36,13 @@ function ProblemPage() {
         <PanelGroup direction="horizontal">
           {/* left panel- problem desc */}
           <Panel defaultSize={40} minSize={30}>
-            <ProblemDescription />
+            {/*We are passing props to the ProblemDescription.jsx page */}
+            <ProblemDescription
+              problem={currentProblem}
+              currentProblemId={currentProblemId}
+              onProblemChange={handleProblemChange}
+              allProblems={Object.values(PROBLEMS)}
+            />
           </Panel>
           <PanelResizeHandle className="w-2 bg-base-300 hover:bg-primary transition-colors cursor-col-resize" />
           {/* right panel- code editor & output */}
