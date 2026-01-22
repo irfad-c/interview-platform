34d024ef19ab75c23875e54a89836e86675144d4
Irfad C <irfadc500@gmail.com>
2026-01-22 12:21:58 +0530

    Implement dashboard page with session management and data fetching

---
+++ frontend/src/components/StatsCards.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function StatsCards() {
+  return (
+    <div>StatsCards</div>
+  )
+}
+
+export default StatsCards
---
+++ frontend/src/components/WelcomeSection.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function WelcomeSection() {
+  return (
+    <div>WelcomeSection</div>
+  )
+}
+
+export default WelcomeSection
