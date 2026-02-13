0f4b901c2f1d5e45e29311b532eba0d14db06b20
Irfad C <irfadc500@gmail.com>
2026-02-13 21:26:24 +0530

    Implement video call and chat UI feature in the session page

---
+++ frontend/src/components/VideoCallUI.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function VideoCallUI() {
+  return <div>VideoCallUI</div>;
+}
+
+export default VideoCallUI;
---
+++ frontend/src/hooks/useStreamClient.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function useStreamClient() {
+  return <div>useStreamClient</div>;
+}
+
+export default useStreamClient;
